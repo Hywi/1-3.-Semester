d2da4bcfd145f677eac61fa6dad15e0a81378c84
Jaco Sørensen <jsso39496@edu.ucl.dk>
2022-10-25 09:16:37 +0200

    added elmåler

---
+++ ELAPI/ELAPI/Model/Elmåler.cs
@@ -0,0 +1,13 @@
+﻿namespace ELAPI.Model
+{
+    public class Elmåler
+    {
+        public long MeterId { get; set; }
+        public DateTime FromDateTime { get; set; }
+        public DateTime ToDateTime { get; set; }
+        public double Value { get; set; }
+        public double ElSpotPrice { get; set; } //skal ikke kunne sette den,
+                                                //skal fås fra elspotpris.dk
+        public double TotalPrice { get; set; }  //Value*ElSpotPrice
+    }
+}
