c45d18edd7f0b3bccf3a76062c56202997688eaf
Vuong <vuonquocnguyen2604@gmail.com>
2022-10-25 09:19:10 +0200

    added ElectricityMeter class

---
+++ ELAPI/1SemesterConsoleApp/ElectricityMeter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1SemesterConsoleApp
+{
+    internal class ElectricityMeter
+    {
+        public long MeterId { get; set; }
+
+        public DateTime FromTimeDate { get; set; }
+
+        public DateTime ToTimeDate { get; set; }
+
+        public double Value { get; set; }
+
+        public double ElspotPrice { get; set; }
+
+        public double TotalPrice { get; set; }
+
+        public void ReadElectricityMeter()
+        {
+
+        }
+}
+}
